4e0b7299368bb47ec2c818d08b9ebb53c472811c
oeawfebe <oeawfebe@tutanota.com>
2021-06-02 21:54:27 -0400

    auto

--- contracts/Fundraiser.sol
+++ contracts/Fundraiser.sol
@@ -1,7 +1,18 @@
-pragma solidity 0.5.16;
-contract Fundraiser {
+pragma solidity >0.4.23 <0.7.0;
+import "../openzeppelin-contracts-2.4.0-beta.0/contracts/ownership/Ownable.sol";
+contract Fundraiser is Ownable {
     string public name;
-    constructor(string memory _name) public {
+    string public url;
+    string public imageURL;
+    string public description;
+    address payable public beneficiary;
+    address public custodian;
+    constructor(string memory _name, string memory _url, string memory _imageURL, string memory _description, address payable _beneficiary, address _custodian) public {
         name=_name;
+        url=_url;
+        imageURL=_imageURL;
+        description=_description;
+        beneficiary=_beneficiary;
+        custodian=_custodian;
     }
 }
--- test/fundraiser_test.js
+++ test/fundraiser_test.js
@@ -1,16 +1,63 @@
 const FundraiserContract=artifacts.require("Fundraiser");
+
 contract("Fundraiser",accounts=>{
     let fundraiser;
     const name="Beneficiary Name";
+    const url='beneficiaryname.org';
+    const imageURL="https://placekitten.com/600/350";
+    const description="Beneficiary description";
+    const beneficiary=accounts[1];
+    const custodian=accounts[0];
+
+    const owner=accounts[0];
+    
     describe("initialization",()=>{
         beforeEach (async ()=>{
-            fundraiser= await FundraiserContract.new(name);
-
-
-
+            fundraiser= await FundraiserContract.new(
+                name,
+                url,
+                imageURL,
+                description,
+                beneficiary,
+                custodian,
+                // owner
+            );
         });
+
         it("gets the beneficiary name",async()=>{
+            const actual=await fundraiser.name();
             assert.equal(actual,name,"names should match");
         });
+
+        it("gets the beneficiary url",async()=>{
+            const actual=await fundraiser.url();
+            assert.equal(actual,url,"url should match");
+        });
+
+        it("gets the beneficiary image url",async()=>{
+            const actual=await fundraiser.imageURL();
+            assert.equal(actual,imageURL,"imageURL should match");
+        });
+
+        it("gets the beneficiary description",async()=>{
+            const actual=await fundraiser.description();
+            assert.equal(actual,description,"beneficiary description should match");
+        });
+
+        it("gets the beneficiary",async()=>{
+            const actual=await fundraiser.beneficiary();
+            assert.equal(actual,beneficiary,"beneficiary address should match");
+        });
+
+        it("gets the custodian",async()=>{
+            const actual=await fundraiser.custodian();
+            assert.equal(actual,custodian,"custodian address should match");
+        });
+
+        it("gets the owner",async()=>{
+            const actual=await fundraiser.owner();
+            assert.equal(actual,owner,"owner should match");
+        });
+
     });
 });
